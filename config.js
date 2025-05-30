53d5c81fdadafe170986680ba475ddd25a13b84c
Qeroo10 <mos666xu@gmail.com>
2025-05-30 18:57:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ const fsm = require('fs'); if (fsm.existsSync('config.env')) require('dotenv').c
 ╚═╝░░░░░╚═╝╚═════╝░ 𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 𝗠𝗥 𝗗𝗜𝗟𝗔 𝗢𝗙𝗖
 */
 
-SESSION_ID: process.env.SESSION_ID || "", 
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "MONEY-HEIST-MD-OQNSWJiT#ibLOjL-_HiS4t4MyXIx5gJNBB-Eb9CsGHfrEic6J8S0-DILALK", 
+MONGODB: process.env.MONGODB || "mongodb+srv://altrkhyah5:ovKipi4Co70RLyLY@cluster0.cbhyuzk.mongodb.net/",
 
 
 
